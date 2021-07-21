04a0ed2f7e40f7320867a4813bb648cd7a4872a9
KIrillPal <kirpall@ya.ru>
2021-07-21 21:38:57 +0300

    Created README.md

---
+++ README.md
@@ -0,0 +1,18 @@
+# SortComparation
+Это школьное исследование эффективности разных сортировок на разных объемах данных и устройствах. 
+Приведённые сортировки:
+- Merge sort
+- Radix sort
+- Shell sort
+- Quick sort
+- std::sort
+- std::qsort
+
+Исследование для Quick sort и Merge sort было проведено на двух машинах: ноутбуке и телефоне исследвателя. 
+Итоговые данные сведены в 2 таблицы: 
+[сортировки-виды](https://drive.google.com/file/d/1BXEUk8k15jIN818TtqGtDKOb2HQCAfsH/view?usp=sharing) и 
+[сортировки-устройства](https://drive.google.com/file/d/1mxl2C4l8sRnfmMvnJhYgUqr6fEM1Smtl/view?usp=sharing). 
+Они же имеются на Github в репозитории.
+В файле [sorts.cpp](https://github.com/KIrillPal/SortComparation/blob/main/sorts.cpp) можно посмотреть реализацию самих сортировок.
+# Особенности проекта
+Не знаю, почему это оказалось на Github. Однако, я думаю, этот проект может показать моё умение реализовывать и тестировать сортировки или иные подобные алгоритмы.
--- sorts.cpp
+++ sorts.cpp
@@ -3,11 +3,6 @@
 #include <cmath>
 
 using namespace std;
-#include <iostream>
-#include <ctime>
-#include <cmath>
-
-using namespace std;
 
 int t = 0;
 void qsort(int* A, int l, int r) {
@@ -19,13 +14,9 @@ void qsort(int* A, int l, int r) {
 		t++;
 	}
 	else if (r - l + 1 > 2) {
-		//cout << " Получил "<< r-l+1 << ":";
-		//for (int i = l; i < r+1; i++)
-		//cout << " " << A[i];
 		int lo = l, hi = r, piv = A[l + (r - l) / 2];
 		t++;
 		while (lo <= hi) {
-			//cout << " в " << lo << " " << hi << " ";
 			while (A[lo] < piv) lo++;
 			while (A[hi] > piv) hi--;
 			t += 2;
@@ -36,7 +27,6 @@ void qsort(int* A, int l, int r) {
 				t += 3;
 			}
 		}
-		//cout << endl << "bnd = " << bnd << endl;
 		qsort(A, l, hi);
 		qsort(A, hi + 1, r);
 	}
@@ -62,7 +52,6 @@ void merge_sort(int* a, int* b, int l, int r, bool reversed) {
 		}
 	else if (r - l + 1 > 2) {
 		int mid = (l + (r - l) / 2);
-		//cout << "Вошел" << endl;
 		int v1 = l, v2 = mid + 1, v = l;
 		merge_sort(b, a, l, mid, !reversed);
 		merge_sort(b, a, mid + 1, r, !reversed);
@@ -92,18 +81,11 @@ int main() {
 			int* arr1 = new int[n];
 			int* arr2 = new int[n];
 			int* arr3 = new int[n];
-			//arr2 =new int[n],
-			//arr3 =new int[n];
-			//cout<<" Массив:";
-			//srand(0);
 
 			for (int i = 0; i < n; i++) {
 				arr1[i] = int(rand());
 				arr2[i] = arr1[i];
-				//cin » arr[i];
-				//cout << " " << arr1[i];
 			}
-			//cout << endl << endl << " Сортировка..." << endl;
 			auto first = clock();
 			t = 0;
 			qsort(arr1, 0, n - 1);
@@ -111,15 +93,6 @@ int main() {
 			first = clock(); t = 0;
 			merge_sort(arr2, arr3, 0, n - 1, false);
 			cout << i << ". merge: " <<(clock() - first) << " " << t << endl << endl;
-			//cout << endl << " Массив 1:";
-			//for (int i=0; i<n; i++) {
-			//cout << " " << arr1[i];
-			//}
-			//cout << endl << endl << " Массив 2:";
-			//for (int i=0; i<n; i++) {
-			//cout << " " << arr3[i];
-			//}
-			//cout << endl << endl << endl;
 		}
 	}
 }
